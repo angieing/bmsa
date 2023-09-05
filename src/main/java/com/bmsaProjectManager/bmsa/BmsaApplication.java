7698676fc7a02ffd4bc5f4d639d97144f7c45e93
Angie <angiedanielalf@gmail.com>
2023-09-04 20:28:29 -0500

    Crear un repositorio local

---
+++ src/main/java/com/bmsaProjectManager/bmsa/BmsaApplication.java
@@ -0,0 +1,13 @@
+package com.bmsaProjectManager.bmsa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BmsaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BmsaApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/bmsaProjectManager/bmsa/controller/test.java
@@ -0,0 +1,26 @@
+package com.bmsaProjectManager.bmsa.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+@RequestMapping("/angie")
+public class test {
+    //Esto es una variable que es parte de la clase test
+
+    /*Esto es una variable que es parte de la clase test */
+    String hola = "hola esto es una variable";
+
+    /*Eso es un método que va imprimir el mensaje imprimiré algo */
+    public static void hacerAlgo(){
+        System.out.println("Imprimire algo");
+    }
+
+    //Es un método con petición GET llamado desde la clase controlador
+    @RequestMapping(value = "status", method = RequestMethod.GET)
+    public String status() {
+        System.out.println("Angie me mando a imprimir algo en log de seguimiento.");
+		return "OK-saludando a Angie";
+	}
+
+}
